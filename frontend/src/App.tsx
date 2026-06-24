bbec848f75b05eb5037d4c0034f062b6ca84651b
Mo Hamdy <mohamdy@Mos-MacBook-Pro.local>
2026-06-24 09:48:40 -0400

    cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,32 @@
 import { Button } from "@mui/material";
 import "./App.css";
-import { JobsForm } from "./jobs-form/jobs-form";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Navigate, Route, Routes } from "react-router-dom";
 import { ApplicationDashboard } from "./pages/application-dashboard";
 import { NewApplicationPage } from "./pages/new-application-page";
+import { HomePage } from "./pages/home-page";
 
 function App() {
   return (
     <BrowserRouter>
       <nav>
-        <Button variant="outlined">
-          <Link to="/home">Home</Link>
+        <Button component={Link} to="/home" variant="outlined">
+          Home
         </Button>
-        <Button variant="outlined">
-          <Link to="/applications">Application Dashboard</Link>
+        <Button component={Link} to="/applications" variant="outlined">
+          Application Dashboard
         </Button>
-        <Button variant="outlined">
-          <Link to="/new-application">Submit a new application</Link>
+        <Button component={Link} to="/new-application" variant="outlined">
+          Submit a new application
         </Button>
       </nav>
       <div className="table-header">
         <h1 style={{ flex: 1 }}>Job Application Tracker</h1>
       </div>
       <Routes>
-        <Route path="/home" />
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/applications" element={<ApplicationDashboard />} />
-        <Route path="new-application" element={<NewApplicationPage />} />
+        <Route path="/new-application" element={<NewApplicationPage />} />
       </Routes>
     </BrowserRouter>
   );
